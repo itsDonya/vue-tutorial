325ef396eac7d0c50007ef6143bb5d22db92c43e
Donya <thisis2nya@gmail.com>
2022-10-10 09:15:14 +0330

    Contacts simple app got styled

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
-  <h1>My Contacts</h1>
-  <my-contact></my-contact>
+  <h1 class="title">My Contacts</h1>
+  <div class="contacts">
+    <my-contact></my-contact>
+    <my-contact></my-contact>
+    <my-contact></my-contact>
+  </div>
 </template>
 
 <script></script>
 
-components: { MyContact },
+<style>
+.title {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  text-align: center;
+}
+</style>
--- src/components/MyContact.vue
+++ src/components/MyContact.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="contact">
     <h4 class="name">{{ friend.name }}</h4>
     <button class="details-btn" @click="isDetailsVisible = !isDetailsVisible">
       {{ isDetailsVisible ? "Hide" : "Show" }} Details
     </button>
-    <ul v-if="isDetailsVisible">
+    <ul class="details" v-if="isDetailsVisible">
       <li class="phone">{{ friend.phone }}</li>
       <li class="email">{{ friend.email }}</li>
     </ul>
@@ -25,3 +25,53 @@ export default {
   },
 };
 </script>
+
+<style>
+.contact {
+  width: 310px;
+  height: auto;
+  padding: 1.2rem;
+  background-color: #eeeeee80;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  margin: 1.2rem auto;
+}
+.name {
+  font-size: 22px;
+}
+.details-btn {
+  color: #eeeeee;
+  background-color: #252525;
+  border-radius: 4px;
+  padding: 0.4rem 0.6rem;
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
+}
+.details-btn:hover {
+  color: #252525;
+  background-color: #cccccc;
+}
+.details {
+  width: 80%;
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.6rem;
+  padding: 0;
+}
+.phone,
+.email {
+  width: 100%;
+  color: #252525;
+  background-color: #cccccc;
+  text-align: center;
+  border-radius: 6px;
+  padding: 0.6rem 0;
+  margin: 0;
+}
+</style>
