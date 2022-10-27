dffdf1c14b40eb10bbdc991c5ea13db2647fd896
Donya <thisis2nya@gmail.com>
2022-10-27 19:22:05 +0330

    Creating & Using a Store

--- src/App.vue
+++ src/App.vue
@@ -1,131 +1,20 @@
 <template>
-  <h1 class="title">My Contacts</h1>
-  <div class="contacts">
-    <div id="buttons">
-      <button @click="changeAddContactCmp('add-contact', '#252525')">
-        Light theme
-      </button>
-      <button @click="changeAddContactCmp('add-contact-dark', '#eeeeee')">
-        Dark theme
-      </button>
-    </div>
-    <keep-alive>
-      <component
-        :is="addContactCmp"
-        @add-contact="addContact"
-        @form-invalid="formInvalid"
-      >
-        <FormTitle :colorHex="addContactTitleColor" />
-      </component>
-    </keep-alive>
-    <ShowError v-if="formIsInvalid">
-      <h1>Please fill out the form below!</h1>
-    </ShowError>
-    <ContactsContainer>
-      <template v-slot:myContact>
-        <MyContact
-          v-for="friend in friends"
-          :key="friend.id"
-          :id="friend.id"
-          :name="friend.name"
-          :phone-number="friend.phone"
-          :email-address="friend.email"
-          :favorite="friend.favorite"
-          @toggle-favorite="toggleFavorite"
-          :details="friend.detailsVisible"
-          @toggle-details="toggleDetails"
-          @delete-contact="deleteContact"
-        />
-      </template>
-    </ContactsContainer>
-  </div>
+  <MyContainer title="VueX">
+    <h3>{{ $store.state.counter }}</h3>
+    <button @click="upOne">Up 1</button>
+  </MyContainer>
 </template>
 
 <script>
-import MyContact from "./components/MyContact.vue";
-import ContactsContainer from "./components/ContactsContainer.vue";
-import AddContact from "./components/AddContact.vue";
-import AddContactDark from "./components/AddContactDark.vue";
-import FormTitle from "./components/FormTitle.vue";
-import ShowError from "./components/ShowError.vue";
-
+import MyContainer from "./components/MyContainer.vue";
 export default {
   components: {
-    MyContact,
-    ContactsContainer,
-    AddContact,
-    AddContactDark,
-    FormTitle,
-    ShowError,
-  },
-  data() {
-    return {
-      counter: 0,
-      friends: [],
-      addContactTitleColor: "#252525",
-      addContactCmp: "add-contact",
-      formIsInvalid: false,
-    };
+    MyContainer,
   },
   methods: {
-    toggleFavorite(friendId) {
-      const identifiedFriend = this.friends.find(
-        (friend) => friend.id === friendId
-      );
-      identifiedFriend.favorite = !identifiedFriend.favorite;
-    },
-    toggleDetails(friendId) {
-      const identifiedFriend = this.friends.find(
-        (friend) => friend.id === friendId
-      );
-      identifiedFriend.detailsVisible = !identifiedFriend.detailsVisible;
-    },
-    addContact(newContact) {
-      const { contactName, contactPhone, contactEmail, contactFavorite } =
-        newContact;
-      this.friends.unshift({
-        id: String(this.counter),
-        name: contactName,
-        phone: String(contactPhone),
-        email: contactEmail,
-        favorite: contactFavorite,
-        detailsVisible: false,
-      });
-      this.counter++;
-      this.formIsInvalid = false;
-    },
-    deleteContact(friendId) {
-      const identifiedFriend = this.friends.find(
-        (friend) => friend.id === friendId
-      );
-      this.friends.splice(identifiedFriend, 1);
-    },
-    changeAddContactCmp(cmp, color) {
-      this.addContactCmp = cmp;
-      this.addContactTitleColor = color;
-    },
-    formInvalid() {
-      this.formIsInvalid = true;
+    upOne() {
+      this.$store.state.counter++;
     },
   },
 };
 </script>
-
-<style scoped>
-.title {
-  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
-    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
-  text-align: center;
-}
-#buttons {
-  width: 340px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 1.6rem auto;
-}
-#buttons button {
-  padding: 1rem 2.8rem;
-  cursor: pointer;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-createApp(App).mount("#app");
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
+const app = createApp(App);
+
+app.config.globalProperties.$store = store;
+app.use(store);
+
+app.mount("#app");
