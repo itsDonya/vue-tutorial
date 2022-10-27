89f7e711159005738e427a5cba49bbb1433a1627
Donya <thisis2nya@gmail.com>
2022-10-27 19:25:22 +0330

    Connecting Components to State

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <MyContainer title="VueX">
-    <h3>{{ $store.state.counter }}</h3>
+    <TheCounter />
     <button @click="upOne">Up 1</button>
   </MyContainer>
 </template>
 
 <script>
 import MyContainer from "./components/MyContainer.vue";
+import TheCounter from "./components/TheCounter.vue";
 export default {
   components: {
     MyContainer,
+    TheCounter,
   },
   methods: {
     upOne() {
