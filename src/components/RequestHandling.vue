255f931c7d73b6375be3620ec0d7ad5c34154a93
Donya <thisis2nya@gmail.com>
2022-10-28 13:20:11 +0330

    Using Mapper Helpers (Actions)

--- src/components/RequestHandling.vue
+++ src/components/RequestHandling.vue
@@ -1,11 +1,12 @@
 <template>
   <h2>{{ counter }}</h2>
   <button @click="addOne">Add 1</button>
-  <button @click="sendCounterValue">Send Counter Value</button>
-  <button @click="getCounterValue">Get Counter Value</button>
+  <button @click="sendCounterValue(500)">Send Counter Value</button>
+  <button @click="getCounterValue(500)">Get Counter Value</button>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   computed: {
     counter() {
@@ -16,12 +17,7 @@ export default {
     addOne() {
       this.$store.commit("addOne");
     },
-    sendCounterValue() {
-      this.$store.dispatch("sendCounterValue", 500);
-    },
-    getCounterValue() {
-      this.$store.dispatch("getCounterValue", 500);
-    },
+    ...mapActions(["sendCounterValue", "getCounterValue"]),
   },
 };
 </script>
